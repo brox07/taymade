2cac8a6ef31fff762310e2c4f6d1df216ea8bdaa
brandon <brox07@gmail.com>
2024-05-17 10:34:35 -0500

    add landing page, update nav, update site title

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -7,22 +7,25 @@ function MainNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Taylor Made Esthetics</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link href="https://taylorbroxterman.glossgenius.com">Book Here!</Nav.Link>
+            <Nav.Link href="#about-me">About Me</Nav.Link>
+            <NavDropdown title="Socials" id="basic-nav-dropdown">
+              <NavDropdown.Item href="https://tiktok.com/@taylorcoates0">
+                TikTok</NavDropdown.Item>
+              <NavDropdown.Item href="https://instagram.com/taylormadeesthetics07">
+                Instagram</NavDropdown.Item>
+              <NavDropdown.Item href="https://facebook.com/profile.php?id=61553691264389">
+                Facebook</NavDropdown.Item>
               <NavDropdown.Divider />
+              <NavDropdown.Item href="https://search.google.com/local/writereview?placeid=ChIJW0leaMcDv4cRWzQeEHXCT7o">
+                Google Review</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+                Linktree</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
