d4728385a9e59cc8f7d9d97e6fa1dade9859f229
brox07 <brox07@gmail.com>
2024-09-16 13:59:52 -0500

    update carousel and landing page links

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,42 +1,47 @@
 import Carousel from 'react-bootstrap/Carousel';
 import '../styles/LandingPage.css';
 import roller from '../assets/green-roller.jpg';
+import carouselImage1 from '../assets/carousel-image-1.png';
+import carouselImage2 from '../assets/carousel-image-2.png';
+import carouselImage3 from '../assets/carousel-image-3.png';
 
 function LandingCarousel() {
   return (
     <div>
       <Carousel fade>
         <Carousel.Item>
-          <img src={roller} className="carousel-image" alt="image of face roller" />
+          <img src={carouselImage1} className="carousel-image" alt="image of face roller" />
           <Carousel.Caption>
             <div>
-              <p>Book a service with Taylor Made Esthetics today!!</p>
-              <a href="https://taylorbroxterman.glossgenius.com/services"><h1>BOOK HERE!</h1></a>
+              <p><span>Book a service with Taylor Made Esthetics today!!</span></p>
+              <h1><a href="https://taylorbroxterman.glossgenius.com/services"><span>BOOK HERE!</span></a></h1>
             </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img src={roller} className="carousel-image" alt="image of face roller" />
+          <img src={carouselImage2} className="carousel-image" alt="image of face roller" />
           <Carousel.Caption>
-            <div>
-              <p>"Very professional. I am a repeat customer
-                <br/>for life!" - Ish</p>
-              <p>"My first ever facial and it was great!
+            <div className="carousel-image-2">
+              <p><span>"Very professional. I am a repeat customer
+                <br/>for life!" - Ish</span></p>
+              <p><span>"My first ever facial and it was great!
                 <br/>My skin looked and felt fantastic. I cant
-                <br/>wait to go back!" - Alondra</p>
-              <h4>Check out more reviews:</h4>
-              <p><a href="https://www.google.com/maps/place/Taylor+Made+Esthetics,+LLC/@39.0511263,-95.7038793,846m/data=!3m1!1e3!4m8!3m7!1s0x87bf03c7685e495b:0xba4fc275101e345b!8m2!3d39.0511263!4d-95.7013044!9m1!1b1!16s%2Fg%2F11ld5jbxhn?entry=ttu&g_ep=EgoyMDI0MDkxMC4wIKXMDSoASAFQAw%3D%3D">Google Reviews</a>
-                <br/><a href="https://taylorbroxterman.glossgenius.com/about#all-reviews">Gloss Genius Reviews</a></p>
+                <br/>wait to go back!" - Alondra</span></p>
+              <h4><span>Check out more reviews:</span></h4>
+              <p><a href="https://www.google.com/maps/place/Taylor+Made+Esthetics,+LLC/@39.0511263,-95.7038793,846m/data=!3m1!1e3!4m8!3m7!1s0x87bf03c7685e495b:0xba4fc275101e345b!8m2!3d39.0511263!4d-95.7013044!9m1!1b1!16s%2Fg%2F11ld5jbxhn?entry=ttu&g_ep=EgoyMDI0MDkxMC4wIKXMDSoASAFQAw%3D%3D"><span>Google Reviews</span></a>
+                <br/><a href="https://taylorbroxterman.glossgenius.com/about#all-reviews"><span>Gloss Genius Reviews</span></a></p>
             </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img src={roller} className="carousel-image" alt="image of face roller" />
+          <img src={carouselImage3} className="carousel-image" alt="image of face roller" />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3></h3>
+            <p><span>
+              Want to get a service for a loved one?
+              We offer gift cards!
+            </span></p>
+            <h3><a href=""><span>Gift Cards</span></a></h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
